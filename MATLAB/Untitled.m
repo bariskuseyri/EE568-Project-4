ae6963d2bad0d734379c012f88ddb71561643e6a
Barış Kuseyri <bariskuseyri93@gmail.com>
2020-06-26 03:09:29 +0300

    Continue with base machine characteristics calculation
    Base machine characteristics are calculated and reported.

--- MATLAB/Untitled.m
+++ MATLAB/Untitled.m
@@ -16,6 +16,11 @@ p = pp*2;   % number of poles
 
 Drotor_outer = 0.290;               % rotor diameter [m]
 Rrotor_outer = Drotor_outer/2;      % rotor radius [m]
+l_axial = 0.068;                    % axial length [m]
+P_em = 46979;
+w_rpm = 2700;
+w_rad = w_rpm*(2*pi/60);
+T_em = P_em/w_rad;
 
 u_0 = 4 * pi * 1e-7;    % permeability of vacuum
 u_r = 1.05;             % relative permeability of magnets
@@ -48,4 +53,44 @@ for i=1:size(Q,2)
 end
 
 Spp=min(C_T);
+
+
+%% Machine Size
+
+B_load = 0.95;
+A_load = 50000*sqrt(2);
+C_mec = ((pi^2)/2)*A_load*B_load;
+
+sigma_Ftan = B_load*A_load/2;
+
+V_rotor = T_em/(sigma_Ftan*2);
+
+%% Machine Length to air-gap diameter
+
+p = 2:15;
+X_ratio = pi.*p.^(1/2)./(4.*p);
+
+figure(1)
+g1 = stem(2*p,X_ratio);
+g1.Color = 'k';
+ax = gca;
+ax.XGrid = 'off';
+ax.YGrid = 'on';
+% title('Distribution Factor Amplitudes')
+ylabel('{\it\chi_{ratio}}')
+xlabel('{\itnumber of pole-pairs p}')
+xticks(2*p)
+% yticks([0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0])
+% axis([0 22 0 1.1])
+set(gcf,'units','centimeters','position',[2,5,30,6])
+
+%% Air-gap Clearance
+
+l_g = (0.18+0.006*(P_em^(0.4)))/1000;
+
+%% Rotor Diameter
+
+D_r = ((2*V_rotor/pi).*(1./X_ratio)).^(1/3);
+l_axial_eff = (D_r+l_g).*X_ratio;
+l_axial = l_axial_eff - 2*l_g;
 
